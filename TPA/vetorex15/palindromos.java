8ea488edcf7a614d87230d452a61febc0d25b89c
PedrohgPires <142914374+PedrohgPires@users.noreply.github.com>
2023-11-18 19:21:14 -0300

    Update palindromos.java

--- TPA/vetorex15/palindromos.java
+++ TPA/vetorex15/palindromos.java
@@ -4,36 +4,16 @@ public class palindromos{
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
         final int TAM = 10;
-        int A[] = new int[TAM];{
-
-        // Preencha o vetor A
-        for (int i = 0; i < A.length; i++) {
-            System.out.println("Digite o " + (i + 1) + "º número: ");
-            A[i] = leia.nextInt();
+        int a[] = new int[TAM];{
+        	for (int i=0;  i< a.length; i++) {
+    			System.out.println("Digite o "+(i+1)+"termo do vetor a");
+    			a[i] = leia.nextInt();
         }
-
-        for (int i = 0; i < A.length; i++) {
-            int numero = A[i];
-            int reverso = 0;
-            int original = numero;
-
-            if (original < 10) {
-                System.out.println(A[i] + " não é um palíndromo.");
-                continue;
-            }
-
-            while (numero > 0) {
-                int digito = numero % 10;
-                reverso = reverso * 10 + digito;
-                numero /= 10;
-            }
-
-            if (original == reverso) {
-                System.out.println(A[i] + " é um palíndromo.");
-            } else {
-                System.out.println(A[i] + " não é um palíndromo.");
-            }
-        }
-        }
-    }}
-
+        	if(a[0] == a[9] && a[1] == a[8] && a[2] == a[7] && a[3] == a[6] && a[4] == a[5]) {
+        		System.out.println("É um palindromo");
+        	}else {
+        		System.out.println("Não é palindromo");
+        	}
+    }
+}
+}
